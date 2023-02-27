330f89454500170402da1bcc07db4c0e58320b35
Zejian You <zyou6474@sdsu.edu>
2023-02-27 14:28:15 -0500

    correct typo

--- src/read_rstrt.f90
+++ src/read_rstrt.f90
@@ -37,7 +37,7 @@
   open (18,file=trim(cfn)//'/theta.'//cidx,form="unformatted", &
            access="direct",action='read',status='old',recl=length)
         read (18,rec=1) theta
-  write(*,*) '   * Reading w-component... done'
+  write(*,*) '   * Reading theta-component... done'
   close(18)
 
   return
